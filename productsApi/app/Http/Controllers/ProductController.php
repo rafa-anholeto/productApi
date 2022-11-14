bb64314b64bf032ebf9faced7aa652722feadc14
rafa-anholeto <rafaanholeto3@gmail.com>
2022-11-14 12:51:05 -0300

    Projeto productsApi

---
+++ productsApi/app/Http/Controllers/ProductController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Products;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function __construct(Products $product){
+        $this->product = $product;
+
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $products = $this->product->all();
+        return $products;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = Products::create($request->all());
+        return $product;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Products  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Products $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Products  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Products $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Products  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Products $product)
+    {
+        $product->update($request->all());
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Products  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Products $product)
+    {
+        $product->delete();
+
+    }
+}
---
+++ productsApi/database/migrations/2022_11_13_175335_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('sku', 30);
+            $table->string('name', 30);
+            $table->string('category', 130);
+            $table->string('description');
+            $table->string('main_image');
+            $table->string('other_image');
+            $table->integer('stock_number');
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
